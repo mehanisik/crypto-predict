9b0695eadbe70cc72ef5feb030e9e1dbbc75885d
mehanisik <mehmethanifiisik@yahoo.com>
2025-02-01 15:47:41 +0100

    feat: remove unused API call from client

--- client/api.ts
+++
@@ -1,9 +0,0 @@
-// Example API call
-fetch("http://localhost:5000/train", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  credentials: "include",
-  body: JSON.stringify(data),
-});
